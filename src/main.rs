e7118d3a52fd3aa6e872ba87110195d7fbe97e84
Huanghe <2021306939@qq.com>
2023-07-27 02:45:39 +0800

    - fix escape sequence

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use bnf::{Grammar, Term};
 use qp_trie::Trie;
-use std::arch::x86_64::_rdseed16_step;
+use std::borrow::Borrow;
 use std::{collections::*, time::Instant};
 use std::{fs, vec};
 
 pub mod utils {
+    use crate::VecU8Wrapper;
     use bnf::{Grammar, Term};
     use qp_trie::Trie;
     use std::collections::{HashMap, HashSet};
@@ -26,33 +27,102 @@ pub mod utils {
         simplified_grammar
     }
 
-    pub fn read_world_vocab(file_name: &str) -> (Trie<Vec<u8>, u32>, HashMap<u32, String>) {
+    pub fn read_world_vocab(file_name: &str) -> (Trie<VecU8Wrapper, u32>, HashMap<u32, String>) {
         let file = File::open(file_name).expect(format!("{file_name} does not exist.").as_str());
         let reader = BufReader::new(file);
         let mut map: HashMap<u32, String> = HashMap::new();
-        let mut tree = Trie::<Vec<u8>, u32>::new();
+        let mut tree = Trie::<VecU8Wrapper, u32>::new();
         for line in reader.lines() {
             let line = line.unwrap();
-            let s1 = line.find(' ').expect(
+            let mut start = line.find(' ').expect(
                 format!(
                     "Invalid format. Ensure this vocab file{file_name} belongs to RWKV world model."
                 )
                 .as_str(),
             );
-            let s2 = line.rfind(' ').expect(
+            let mut end = line.rfind(' ').expect(
                 format!(
                     "Invalid format. Ensure this vocab file{file_name} belongs to RWKV world model."
                 )
                 .as_str(),
             );
-            let token_id = line[..s1]
+            let token_id = line[..start]
                 .parse::<u32>()
                 .expect(format!("{line} cannot be parsed.").as_str());
-            let token = line[s1 + 1 + 1..s2 - 1].trim();
-            tree.insert(token.bytes().into_iter().collect(), token_id);
-            map.insert(token_id, token.to_string().clone());
+            start += 1;
+            end -= 1;
+            if line.chars().nth(start).unwrap() == 'b' {
+                start += 2;
+            } else {
+                start += 1;
+            }
+            // println!("token: {}",&line[start..end]);
+            let token = fix_utf8_escape(&line[start..end]);
+            tree.insert(VecU8Wrapper(token.clone()), token_id);
+            // println!("{:?}", String::from_utf8(token.clone()));
+            map.insert(token_id, String::from_utf8(token).unwrap());
         }
         (tree, map)
+    }
+
+    pub fn fix_utf8_escape(token: &str) -> Vec<u8> {
+        /*
+            translated from https://github.com/npk48/rwkv_cuda/blob/main/tokenizer.hpp#L166
+            sequence need to be unescaped
+            [
+                "\\symbol", ["\\", "symbol"]
+                "\\",       ["\\"]
+                "\\t",      ["\\", "t"]
+                "\\n",      ["\\", "n"]
+                "\\r",      ["\\", "r"]
+                "\\x12",    ["\\", "x", "1", "2"]
+                "\\u1234",  ["\\", "u", "1", "2", "3", "4"]
+            ]
+        */
+
+        let mut result: Vec<u8> = Vec::new();
+        result.reserve(token.as_bytes().len());
+        let mut token = token;
+        let convert_to_utf8 = |c: char, buffer: &mut Vec<u8>| {
+            let mut temp = [0, 0, 0, 0];
+            buffer.extend(c.encode_utf8(&mut temp).as_bytes());
+        };
+        let process_hex_digits = |hex_digit_len: usize, token: &str, buffer: &mut Vec<u8>| {
+            let hex_digits: String = token.chars().skip(2).take(hex_digit_len).collect();
+            convert_to_utf8(
+                char::from_u32(u32::from_str_radix(&hex_digits, 16).unwrap()).unwrap(),
+                buffer,
+            );
+        };
+        while !token.is_empty() {
+            let c = token.chars().next().unwrap();
+            if c == '\\' {
+                let next_c = token.chars().nth(1).unwrap();
+                if next_c == 't' {
+                    result.push(b'\t');
+                    token = &token[2..];
+                } else if next_c == 'n' {
+                    result.push(b'\n');
+                    token = &token[2..];
+                } else if next_c == 'r' {
+                    result.push(b'\r');
+                    token = &token[2..];
+                } else if next_c == 'x' {
+                    process_hex_digits(2, token, &mut result);
+                    token = &token[4..];
+                } else if next_c == 'u' {
+                    process_hex_digits(4, token, &mut result);
+                    token = &token[6..];
+                } else {
+                    result.push(next_c as u8);
+                    token = &token[2..];
+                }
+            } else {
+                convert_to_utf8(c, &mut result);
+                token = &token[c.len_utf8()..];
+            }
+        }
+        result
     }
 }
 #[derive(PartialEq, Clone, Debug, Copy)]
@@ -61,11 +131,32 @@ pub enum StackItem<'a> {
     Terminal(&'a str),
     Byte(u8),
 }
+pub struct VecU8Wrapper(Vec<u8>);
 
+impl Borrow<[u8]> for VecU8Wrapper {
+    #[inline]
+    fn borrow(&self) -> &[u8] {
+        self.0.as_slice()
+    }
+}
+
+impl qp_trie::Break for VecU8Wrapper {
+    type Split = [u8];
+
+    #[inline]
+    fn empty<'a>() -> &'a [u8] {
+        <&'a [u8]>::from(<&'a [u8]>::default())
+    }
+
+    #[inline]
+    fn find_break(&self, mut loc: usize) -> &[u8] {
+        From::from(&self.0[..loc])
+    }
+}
 pub struct PushDownAutomata<'a> {
     stacks: Vec<Vec<StackItem<'a>>>,
     grammar: HashMap<&'a str, HashSet<Vec<&'a Term>>>,
-    tokens_tree: &'a Trie<Vec<u8>, u32>,
+    tokens_tree: &'a Trie<VecU8Wrapper, u32>,
 }
 
 enum BytesMatchResult<'b> {
@@ -79,7 +170,7 @@ impl<'a> PushDownAutomata<'a> {
     pub fn new(
         grammar: &'a Grammar,
         start_term: &'a Term,
-        tokens_tree: &'a Trie<Vec<u8>, u32>,
+        tokens_tree: &'a Trie<VecU8Wrapper, u32>,
     ) -> PushDownAutomata<'a> {
         let start_nonterminal = match start_term {
             Term::Nonterminal(x) => x,
@@ -97,22 +188,36 @@ impl<'a> PushDownAutomata<'a> {
     pub fn all_possible_next_tokens<'b>(
         &mut self,
         previous_tokens: Option<&'b [u8]>,
-    ) -> HashSet<u32> {
+    ) -> Option<HashSet<u32>> {
         let now = Instant::now();
-        self.accept_tokens(previous_tokens);
-        println!("Time used for accepting tokens: {:?}",now.elapsed());
+        if !self.accept_tokens(previous_tokens) {
+            return None;
+        }
+        println!("Time used for accepting tokens: {:?}", now.elapsed());
         let mut token_ids: HashSet<u32> = HashSet::new();
         let mut current_stack: Vec<StackItem> = vec![];
-        for (prefix, stack) in
-            self.stacks
-                .iter()
-                .map(|x| match x.last().expect("No stack should be empty.") {
-                    StackItem::Byte(byte) => (*byte, x),
-                    _ => panic!("The top item of a stack should be byte."),
-                })
-        {
+        for (prefix, stack) in self.stacks.iter().map(|x| {
+            let mut index = 0;
+            let mut temp = vec![];
+            for i in (0..x.len()).rev() {
+                match x[i] {
+                    StackItem::Byte(_) => index = i,
+                    _ => break,
+                }
+            }
+            for i in (&x[index..x.len()]).into_iter().rev() {
+                match i {
+                    StackItem::Byte(value) => temp.push(*value),
+                    _ => panic!("Only bytes here."),
+                }
+            }
+            (temp, x)
+        }) {
             let now = Instant::now();
-            for (token, &token_id) in self.tokens_tree.iter_prefix([prefix].as_slice()) {
+            for (VecU8Wrapper(token), &token_id) in self.tokens_tree.iter_prefix(
+                self.tokens_tree
+                    .longest_common_prefix(&VecU8Wrapper(prefix)),
+            ) {
                 if token_ids.contains(&token_id) {
                     continue;
                 }
@@ -130,22 +235,22 @@ impl<'a> PushDownAutomata<'a> {
                 }
                 current_stack.clear();
             }
-            println!("Time used for one stack: {:?}",now.elapsed());
+            println!("Time used for one stack: {:?}", now.elapsed());
         }
-        token_ids
+        Some(token_ids)
     }
-
-    fn accept_tokens<'b>(&mut self, bytes: Option<&'b [u8]>) {
+    #[must_use]
+    fn accept_tokens<'b>(&mut self, bytes: Option<&'b [u8]>) -> bool {
         let len = self.stacks.len();
-        let mut find_stacks_matching_bytes = |bytes|
-        {
-            let mut stack:Vec<StackItem> = Vec::new();
-            stack.reserve(self.stacks.iter().map(|x|x.len()).max().unwrap());
+        let mut find_stacks_matching_bytes = |bytes| {
+            let mut stack: Vec<StackItem> = Vec::new();
+            let mut accepted = false;
+            stack.reserve(self.stacks.iter().map(|x| x.len()).max().unwrap());
             for i in 0..len {
                 stack.extend(&self.stacks[i]);
                 match stack.last() {
                     Some(_) => {
-                        Self::find_stacks_matching_bytes(
+                        accepted |= Self::find_stacks_matching_bytes(
                             &mut stack,
                             &self.grammar,
                             bytes,
@@ -162,17 +267,18 @@ impl<'a> PushDownAutomata<'a> {
                 };
                 stack.clear();
             }
-            for i in (0..len).rev()
-            {
+            for i in (0..len).rev() {
                 self.stacks.swap_remove(i);
             }
+            accepted
         };
         if bytes.is_some() {
-            find_stacks_matching_bytes(bytes);
+            if !find_stacks_matching_bytes(bytes) {
+                return false;
+            }
         }
-        find_stacks_matching_bytes(None);
+        find_stacks_matching_bytes(None)
         // println!("{:?}", self.stacks);
-        
     }
 
     fn convert_terminal_to_bytes(stack: &mut Vec<StackItem<'a>>, popped_terminal: &str) {
@@ -300,13 +406,15 @@ impl<'a> PushDownAutomata<'a> {
 
 fn main() {
     let input = fs::read_to_string("./grammar.bnf").expect("grammar.bnf should exist.");
+    let input = String::from_utf8(utils::fix_utf8_escape(&input)).unwrap();
     let grammar: Grammar = input.parse().unwrap();
     let (tree, map) = utils::read_world_vocab("vocab.txt");
     // println!("{:#?}", Utils::SimplifyGrammarTree(&grammar));
     let binding = Term::Nonterminal("dna".to_string());
     let mut machine = PushDownAutomata::new(&grammar, &binding, &tree);
-    let mut result: Vec<&str> = machine
+    let result: Vec<&str> = machine
         .all_possible_next_tokens(None)
+        .unwrap()
         .into_iter()
         .map(|x| map[&x].as_str())
         .collect();
@@ -322,13 +430,15 @@ fn main() {
             .expect("Input should exist");
         let now = Instant::now();
         // println!("{:?}", machine.stacks);
-        let input = input.trim();
-        let result: Vec<&str> = machine
-            .all_possible_next_tokens(Some(&input.as_bytes()))
-            .into_iter()
-            .map(|x| map[&x].as_str())
-            .collect();
-        // println!("{:?}", machine.stacks);
+        let input = utils::fix_utf8_escape(input.trim());
+        let result: Vec<&str> = match machine.all_possible_next_tokens(Some(&input)) {
+            Some(result) => result.into_iter().map(|x| map[&x].as_str()).collect(),
+            None => {
+                println!("Invalid input.");
+                break;
+            }
+        };
+        println!("{:?}", machine.stacks);
         let end = now.elapsed();
         times.push(end.as_secs_f64());
         println!("Time used: {:?}", end);
